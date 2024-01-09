3dc3f4210731202e5ffa0ad8f46d1ea07acaf3e6
Adem Bouaicha <151208515+adembix@users.noreply.github.com>
2024-01-09 02:46:29 +0100

    Add files via upload

---
+++ OOP-With-CPP/FriendClass.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class clsPerson {
+private:
+    int var1 = 10;
+public:
+    int var2 = 20;
+
+    friend class clsEmployee;
+};
+
+class clsEmployee : public clsPerson {
+public:
+    void varPrint(){
+        cout << var1 << endl; //  accessible private member for base class
+    }
+};
+
+
+int main() {
+
+    clsEmployee obj;
+
+    obj.varPrint();
+
+    return 0;
+}
